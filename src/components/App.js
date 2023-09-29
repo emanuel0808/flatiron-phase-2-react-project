ed3050262ae38134735d1a8222eea8d81a47c6b2
Emanuel Gaona <gaonaemanuel7@gmail.com>
2023-09-29 14:54:33 -0700

    modified my components

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import Header from './components/Header';
-import MealPlan from './components/MealPlan';
-import Footer from './components/Footer';
-import MealItem from './components/MealItem'; // Import MealItem component from the "components" directory
-import MealCard from './components/MealCard';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './Header';
+import MealPlan from './MealPlan';
+import Footer from './Footer';
+import MealCard from './MealCard';
+import Main from './Main'; // Import the Main component for routing
+
 function App() {
   const [mealPlan, setMealPlan] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/mealPlan/week') 
+    // Fetch meal plan data from your API or source
+    fetch('http://localhost:3000/mealPlan')
       .then((response) => response.json())
       .then((data) => setMealPlan(data))
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
   return (
-    <div>
-      <Header />
-      <MealPlan mealPlanData={mealPlan} />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <MealPlan mealPlanData={mealPlan} />
+          </Route>
+          <Route path="/about">
+            {/* Add your About component here */}
+          </Route>
+          <Route path="/contact">
+            {/* Add your Contact component here */}
+          </Route>
+        </Switch>
+        <Footer />
+        <div className="meal-card-container">
+          {mealPlan.map((dayData, index) => (
+            <MealCard
+              key={index}
+              day={dayData.day}
+              image={dayData.image}
+              meals={dayData.meals}
+            />
+          ))}
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
   return (
-    <header>
-      <h1>Meal Plan App</h1>
+    <header className="header">
+      <h1>Meal-Prep App</h1>
+      <p>Are you ready to embark on a journey to a healthier, happier you? Introducing MealPrepPro, your ultimate companion on the path to successful weight loss and better eating habits.</p>
+      <nav>
+        <ul className="nav-links">
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
     </header>
   );
 }

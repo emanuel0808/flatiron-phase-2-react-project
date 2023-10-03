09fe5c3c2045888c275dd989966c781869f94ebb
Emanuel Gaona <gaonaemanuel7@gmail.com>
2023-10-02 20:21:08 -0700

    adding data in my Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,11 @@ function Home() {
         Start your journey to a healthier lifestyle with our carefully crafted meal plans.
         Whether you're looking to shed a few pounds or simply eat better, we've got you covered.
       </p>
+      <p>
+        Our mission is to make healthy eating easy and enjoyable. We understand that maintaining
+        a balanced diet can be challenging in today's busy world, so we've created meal plans that
+        are not only nutritious but also delicious.
+      </p>
     </div>
   );
 }

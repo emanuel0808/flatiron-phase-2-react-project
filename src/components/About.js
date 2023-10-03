37c2a3c38ae8e9c4107ada7fa38d74b3a371ae7e
Emanuel Gaona <gaonaemanuel7@gmail.com>
2023-10-02 20:13:45 -0700

    adding more data in my about.js

--- src/components/About.js
+++ src/components/About.js
@@ -9,6 +9,11 @@ function About() {
         Welcome to our Meal-Prep App! Our mission is to help you achieve your health and fitness goals
         by providing delicious and nutritious meal plans designed for weight loss and overall well-being.
       </p>
+      <p>
+        What sets us apart? We offer a wide variety of meal plans tailored to your dietary preferences,
+        including vegetarian, vegan, gluten-free, and more. Our recipes are carefully crafted by
+        nutrition experts to ensure you get the right balance of nutrients, without sacrificing flavor.
+      </p>
     </div>
   );
 }

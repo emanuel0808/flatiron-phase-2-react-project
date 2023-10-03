41187a544a6045e5cb6a5fc709941d74bf071d49
Emanuel Gaona <gaonaemanuel7@gmail.com>
2023-10-02 21:01:13 -0700

    adding data in my sub-header

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,15 @@
+// About.js
 import React from 'react';
 
 function About() {
   return (
-    <div className="about-section">
-      <h2>About Us</h2>
+    <div>
+      <h1>About Us</h1>
       <p>
-        Welcome to MealPrepPro, your trusted partner on your journey to a healthier lifestyle. We are passionate about helping you achieve your health and fitness goals through delicious and nutritious meal plans.
+        Welcome to our Meal-Prep App! Our mission is to help you achieve your health and fitness goals
+        by providing delicious and nutritious meal plans designed for weight loss and overall well-being.
       </p>
-      <p>
-        Our team gwork tirelessly to create meal plans that are not only tasty but also tailored to your specific needs. Whether you're looking to shed a few pounds, gain muscle, or simply improve your overall well-being, we've got you covered.
-      </p>
-      <p>
-        At MealPrep-App, we believe that eating well should be enjoyable, convenient, and sustainable. That's why we provide you with a variety of meal options that are easy to prepare and fit seamlessly into your busy lifestyle.
-      </p>
-      <p>
-        Join us on this exciting journey towards better health and discover the joy of eating well. Your health and well-being are our top priorities, and we can't wait to be part of your success story.
-      </p>
+     
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,37 @@
-
 import React from 'react';
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to Meal-Prep App</h1>
+    <div className="home-section">
+      <h2>Welcome to Our Meal-Prep App</h2>
       <p>
-        Start your journey to a healthier lifestyle with our carefully crafted meal plans.
-        Whether you're looking to shed a few pounds or simply eat better, we've got you covered.
-        
+        Start your journey to a healthier lifestyle with our carefully crafted meal plans. Whether you're looking to shed a few pounds, gain muscle, or simply eat better, we've got you covered.
       </p>
-    
+      <p>
+        MealPrepPro is your ultimate companion on the path to successful weight loss and better eating habits. Our mission is to make healthy eating convenient, delicious, and accessible to everyone.
+      </p>
+      <p>
+        How does it work? It's simple! Just follow these steps:
+      </p>
+      <ol>
+        <li>Sign up for MealPrepPro and create your personalized profile.</li>
+        <li>Explore our wide range of meal plans designed by our team of expert chefs and nutritionists.</li>
+        <li>Choose the plan that suits your dietary preferences and goals.</li>
+        <li>Receive a weekly meal plan, complete with easy-to-follow recipes and a shopping list.</li>
+        <li>Hit the kitchen and prepare delicious, healthy meals with our step-by-step instructions.</li>
+        <li>Enjoy your meals, knowing you're fueling your body with the best ingredients.</li>
+      </ol>
+      <p>
+        With MealPrepPro, you'll discover the joy of eating well without the stress of meal planning and grocery shopping. Our meal plans are designed to save you time and help you make better food choices.
+      </p>
+      <p>
+        Join the thousands of individuals who have transformed their lives with MealPrepPro. We're here to support you every step of the way on your journey to a healthier, happier you.
+      </p>
+      <p>
+        Get started today and take the first step towards a brighter, healthier future. Your health and well-being are our top priorities.
+      </p>
     </div>
   );
 }
 
 export default Home;
-

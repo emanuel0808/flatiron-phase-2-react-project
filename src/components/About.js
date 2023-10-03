ba9722ba77f76655d471e994b703d0d137d10983
Emanuel Gaona <gaonaemanuel7@gmail.com>
2023-10-02 21:39:57 -0700

    added more data in my about.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,6 +8,8 @@ function About() {
       <p>
         Welcome to our Meal-Prep App! Our mission is to help you achieve your health and fitness goals
         by providing delicious and nutritious meal plans designed for weight loss and overall well-being.
+
+        Our commitment to your well-being extends beyond the kitchen. We believe that a healthy lifestyle should be enjoyable, sustainable, and empowering. MealPrepPro is here to inspire and support you on your journey to a happier, healthier you.
       </p>
      
     </div>

b5aad8e5ed4b8e715072ab7bf8e3333dfe7434e0
Emanuel Gaona <gaonaemanuel7@gmail.com>
2023-10-02 21:52:30 -0700

    added more data in my Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,10 @@ function Header() {
   return (
     <header className="header">
       <h1>Meal-Prep App</h1>
-      <p>Are you ready to embark on a journey to a healthier, happier you? Introducing MealPrepPro, your ultimate companion on the path to successful weight loss and better eating habits.</p>
+      <p>Are you ready to embark on a journey to a healthier, happier you? Introducing Meal-Prep APP, your ultimate companion on the path to successful weight loss and better eating habits.
+
+      Our mission is to help you achieve your health and fitness goals by providing delicious and nutritious meal plans designed for weight loss and overall well-being. With Meal-Prep APP, you'll discover a world of culinary delights that not only satisfy your taste buds but also support your journey to a healthier lifestyle.
+      </p>
       <nav>
         <ul className="nav-links">
           <li><a href="/">Home</a></li>
